17f5786abfb11b543211ce4a74110185b6f8d80b
PRABHVEERSINGH17 <146875097+PRABHVEERSINGH17@users.noreply.github.com>
2023-12-01 11:51:17 +0530

    Add files via upload

---
+++ duplicate elements.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter limit:");
+    scanf("%d",&n);
+    int a[n],b[n],i,j;
+    printf("enter elements:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        b[i]=0;
+    }
+    printf("Duplicate elements are......\n");
+    for(i=0;i<n;)
+}
